e4231bc3053d74ada88034fa628f8ac5596e0d26
zhangxuebin <1527949074@qq.com>
2024-03-18 11:02:26 +0800

    易买网后端项目搭建0.0.5base版

---
+++ src/main/java/com/kgc/easybuy/pojo/Category.java
@@ -0,0 +1,24 @@
+package com.kgc.easybuy.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    private int id;
+    private String name;
+    private int parentId;
+    private int type;
+    private String iconClass;
+    private int isDel;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+}
