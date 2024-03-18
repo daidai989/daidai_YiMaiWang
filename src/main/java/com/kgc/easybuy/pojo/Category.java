941253ea23b34576e35d3c12b22c659794fbd443
daidai <1304057714@qq.com>
2024-03-18 11:13:29 +0800

    易买网后端项目搭建0.0.2base版

--- src/main/java/com/kgc/easybuy/pojo/Category.java
+++ src/main/java/com/kgc/easybuy/pojo/Category.java
@@ -1,8 +1,23 @@
 package com.kgc.easybuy.pojo;
 
-/**
- * @author daidai
- */
-public class Category {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    private int id;
+    private String name;
+    private int parentId;
+    private int type;
+    private String iconClass;
+    private int isDel;
+
+    private Date createTime;
 }
