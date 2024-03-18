a8dcd054a39b2c5faa03ee95e88c1d570fe89787
daidai <1304057714@qq.com>
2024-03-18 11:39:37 +0800

    易买网后端项目搭建0.0.2base版

--- src/main/java/com/kgc/easybuy/util/RedisUtil.java
+++ src/main/java/com/kgc/easybuy/util/RedisUtil.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.kgc.easybuy.pojo.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
