ecb96535d0bac04c3e9d4ea535f36a9d67a6679f
daidai <1304057714@qq.com>
2024-03-18 11:16:23 +0800

    易买网后端项目搭建0.0.2base版

---
+++ src/main/java/com/kgc/easybuy/pojo/File.java
@@ -0,0 +1,23 @@
+package com.kgc.easybuy.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class File {
+    private int id;
+    private String filePath;
+    private int productId;
+    private int userId;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+    private int isDelete;
+}
---
+++ src/main/java/com/kgc/easybuy/util/RedisUtil.java
@@ -0,0 +1,39 @@
+package com.kgc.easybuy.util;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.kgc.easybuy.pojo.ResponseMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisUtil {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public ResponseMessage setStrToRedis(String key,Object obj) {
+        String value = JSON.toJSONString(obj);
+        stringRedisTemplate.opsForValue().set(key,value);
+        return new ResponseMessage(200,null,null);
+    }
+
+
+    public ResponseMessage getStrByRedis(String key) {
+        String value = stringRedisTemplate.opsForValue().get(key);
+        Object obj= JSONObject.parseObject(value, Object.class);
+        if (obj != null){
+            return new ResponseMessage(200,"成功",obj);
+        }
+        return new ResponseMessage(201,"失败",null);
+    }
+
+    public ResponseMessage delStrByRedis(String key) {
+        Boolean delete = stringRedisTemplate.delete(key);
+        if (delete){
+            return new ResponseMessage(200,"成功",delete);
+        }
+        return new ResponseMessage(201,"失败",delete);
+    }
+}
