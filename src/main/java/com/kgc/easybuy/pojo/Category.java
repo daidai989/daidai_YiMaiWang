d3dbec31a63e9d7bf4d3c6e47f3349de6498d4b7
zhangxuebin <1527949074@qq.com>
2024-03-18 20:34:07 +0800

    易买网后端项目搭建0.0.2base版

--- src/main/java/com/kgc/easybuy/pojo/Category.java
+++ src/main/java/com/kgc/easybuy/pojo/Category.java
@@ -18,5 +18,7 @@ public class Category {
     private int type;
     private String iconClass;
     private int isDel;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 }
